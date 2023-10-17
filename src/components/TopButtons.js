3bab14285d82ae5fc0cdcd4d50292a8156b8256a
Dushan Bhagya <it21017742@my.sliit.lk>
2023-10-17 23:45:34 +0530

    Now going to add time and location part

--- src/components/TopButtons.js
+++ src/components/TopButtons.js
@@ -5,17 +5,33 @@ function TopButtons() {
     {
         id: 1,
         title: 'Colombo'
-    }
-  ]
+    },
+    {
+        id: 1,
+        title: 'Trico'
+    },
+    {
+        id: 1,
+        title: 'Kandy'
+    },
+    {
+        id: 1,
+        title: 'Galle'
+    },
+    {
+        id: 5,
+        title: 'Jaffna'
+    },
+  ];
 
-  return <div className='flex items-center justify-center my-6'>
-    {cities.map((city) => {
-      return <button className='text-white text-lg font-medium'>
-        {city.title}
-      </button>
-    })}
-  </div>
-
+ return (
+    <div className="flex items-center justify-around my-6">
+        {cities.map((city)=>
+            <button key={city.id} className="text-white text-lg font-medium">
+                {city.title}
+            </button>)}
+    </div>
+ )
 }
 
 export default TopButtons;

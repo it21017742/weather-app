192537857d256efd71fdf5210ed90f05b6b5cd03
Dushan Bhagya <it21017742@my.sliit.lk>
2023-10-18 03:23:48 +0530

    sleepy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import TemperatureAndDetails from './components/TemperatureAndDetails';
 
 function App() {
   return (
-    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 t0-blue-700 h-fit shadow-xl shadow-gray-400">
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButtons></TopButtons>
       <Inputs></Inputs>
 

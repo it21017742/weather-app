7c9ab576e5b2667193965a6012eb2e48b751bd86
Dushan Bhagya <it21017742@my.sliit.lk>
2023-10-18 00:24:08 +0530

    gonna start Temperature and Details

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import TopButtons from './components/TopButtons';
 import Inputs from './components/Inputs';
 import TimeAndLocation from './components/TimeAndLocation';
+import TemperatureAndDetails from './components/TemperatureAndDetails';
 
 function App() {
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 t0-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButtons></TopButtons>
       <Inputs></Inputs>
+
       <TimeAndLocation></TimeAndLocation>
+      <TemperatureAndDetails></TemperatureAndDetails>
     </div>
         
   );
---
+++ src/components/TemperatureAndDetails.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TemperatureAndDetails() {
+  return (
+    <h1>TemperatureAndDetails</h1>
+  )
+}
+
+export default TemperatureAndDetails;
